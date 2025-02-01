b96248aa68f8ca68cfbd73fd7851c33c7325af3e
Maia Neptune <maianeptune@gmail.com>
2025-02-01 00:15:52 -0400

    Initial Schema

---
+++ Schema/Schema.js
@@ -0,0 +1,14 @@
+
+//speaker schema
+{
+    "speaker_id"; String,
+    "first_name"; String,
+    "last_name"; String,
+    "gender"; String,
+    "age_group"; String,
+    "region"; String,
+    "accent"; String, 
+    "language"; String
+}
+
+
