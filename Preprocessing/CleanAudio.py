70bf57c42e79010d13f9979bd44d1c8f9053c7ce
chrissgitt <christophe.gittens@gmail.com>
2025-02-05 18:19:04 -0400

    Audio Cleaning-Service Invoked by Split Audio: API reference

--- Preprocessing/CleanAudio.py
+++ Preprocessing/CleanAudio.py
@@ -18,3 +18,30 @@ class CleanAudio():
         print(outputFile)
 
         wavfile.write(outputFile, rate, reducedNoise)
+
+'''
+
+@article{sainburg2020finding,
+  title={Finding, visualizing, and quantifying latent structure across diverse animal vocal repertoires},
+  author={Sainburg, Tim and Thielk, Marvin and Gentner, Timothy Q},
+  journal={PLoS computational biology},
+  volume={16},
+  number={10},
+  pages={e1008228},
+  year={2020},
+  publisher={Public Library of Science}
+}
+
+@software{tim_sainburg_2019_3243139,
+  author       = {Tim Sainburg},
+  title        = {timsainb/noisereduce: v1.0},
+  month        = jun,
+  year         = 2019,
+  publisher    = {Zenodo},
+  version      = {db94fe2},
+  doi          = {10.5281/zenodo.3243139},
+  url          = {https://doi.org/10.5281/zenodo.3243139}
+}
+
+
+'''
